4a44153c1bd3bb5b29cd9fa96e2a244b1fa05a9b
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-26 10:38:29 +0300

    added the operations functions

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+function addition(a,b) {
+    let c = a + b;
+    return c;
+}
+function subtraction(a,b){
+    let c = a - b;
+    return c;
+}
+function multiplication(a,b){
+    let c = a * b;
+    return c;
+}
+function division(a,b){
+    let c = a / b;
+    return c;
+}
+
+function oparate(params) {
+    
+}
